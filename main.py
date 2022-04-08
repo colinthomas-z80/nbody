add some gravity calculation. is not correct

main.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import time
 
 def main():
-    fig = plt.figure(figsize=(6,6), dpi=80)  
+    fig = plt.figure(figsize=(3,3), dpi=80)  
     dt = 0.01
 
     posr = np.random.randn(10, 2)
     velr = np.random.randn(10, 2)
 
     accr = accelr(posr)
-    for i in range(1000):
+    for i in range(10000):
         velr += accr * dt
         posr += velr * dt
         accr = accelr(posr)
@@ -28,11 +28,18 @@ def main():
 def accelr(posr):
     center = np.array([2.5, 2.5])
     center_mass = 5
+    g = 2
 
     dx = center[0] - posr[:,0:1] # this indexing is required to return as an hstack in the form [[x,y], [x,y], ....]
     dy = center[1] - posr[:,1:2]
 
-    return np.hstack((dx, dy))
+    r = dx**2 + dy**2
+    f = center_mass/abs(r)
+
+    ax = f*g*dx*center_mass
+    ay = f*g*dx*center_mass
+
+    return np.hstack((ax, ay))
 
 # def accel(pos, vel):
 #     center = np.array([2.5, 2.5])