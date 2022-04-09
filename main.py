2e6c499145a14a8c8aa3eeaa7f07ab3a42fb8b00
Colin Thomas <cthomas0687@gmail.com>
2022-04-08 21:46:06 -0400

    improve gravity and disable graph scaling

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import time
 
 def main():
-    fig = plt.figure(figsize=(3,3), dpi=80)  
-    dt = 0.01
+    fig = plt.figure(figsize=(10,10), dpi=80)  
+    plt.autoscale(False)
+    dt = 0.03
 
-    posr = np.random.randn(10, 2)
-    velr = np.random.randn(10, 2)
+    posr = np.random.randn(10000, 2)
+    velr = np.random.randn(10000, 2)
 
     accr = accelr(posr)
     for i in range(10000):
@@ -17,27 +18,29 @@ def main():
 
         plt.cla()
         plt.scatter(posr[:, 0], posr[:, 1], s=10)
-        plt.scatter(2.5,2.5,s=25)
-        plt.xticks(np.arange(0,6))
-        plt.yticks(np.arange(0,6))
-
+        plt.scatter(0,0,s=25)
+        plt.xticks(np.arange(0,10))
+        plt.yticks(np.arange(0,10))
+        plt.xlim(-3, 3)
+        plt.ylim(-3, 3)
         plt.pause(0.001)
 
     plt.show()
 
 def accelr(posr):
-    center = np.array([2.5, 2.5])
-    center_mass = 5
+    center = np.array([0, 0])
+    center_mass = 1
     g = 2
+    padding = 1
 
     dx = center[0] - posr[:,0:1] # this indexing is required to return as an hstack in the form [[x,y], [x,y], ....]
     dy = center[1] - posr[:,1:2]
 
-    r = dx**2 + dy**2
-    f = center_mass/abs(r)
+    r = dx**2 + dy**2 + padding
+    f = center_mass/r**2
 
     ax = f*g*dx*center_mass
-    ay = f*g*dx*center_mass
+    ay = f*g*dy*center_mass
 
     return np.hstack((ax, ay))
 
